f439abc63c9abd92144a12633147d315601d5cda
Paul Hufnagel <pythagorion@web.de>
2023-09-24 13:24:10 +0200

    Translation for german rolebooks added

--- src/@types/predefined.ts
+++ src/@types/predefined.ts
@@ -1,7 +1,7 @@
 import { CombatTechniqueType, Rolebook } from "./char";
 
-export const TheIron: Rolebook = {
-    name: "rolebook.iron",
+export const TheAdamant: Rolebook = {
+    name: "rolebook.adamant",
     stats: {
         creativity: 0,
         focus: 1,
@@ -9,32 +9,600 @@ export const TheIron: Rolebook = {
         passion: 1
     },
     balance: {
-        black: 'rolebook.iron.balance.black',
-        white: 'rolebook.iron.balance.white',
+        black: 'rolebook.adamant.balance.black',
+        white: 'rolebook.adamant.balance.white',
         index: 0
     },
     speciality: {
-        name: 'rolebook.iron.speciality.name',
-        description: 'rolebook.iron.speciality.description'
+        name: 'rolebook.adamant.speciality.name',
+        description: 'rolebook.adamant.speciality.description'
     },
     traits: [
         {
-            name: 'rolebook.iron.traits.0.name',
-            description: 'rolebook.iron.traits.0.description'
+            name: 'rolebook.adamant.traits.0.name',
+            description: 'rolebook.adamant.traits.0.description'
+        },
+        {
+            name: 'rolebook.adamant.traits.1.name',
+            description: 'rolebook.adamant.traits.1.description'
+        },
+        {
+            name: 'rolebook.adamant.traits.2.name',
+            description: 'rolebook.adamant.traits.2.description'
+        },
+        {
+            name: 'rolebook.adamant.traits.3.name',
+            description: 'rolebook.adamant.traits.3.description'
+        },
+        {
+            name: 'rolebook.adamant.traits.4.name',
+            description: 'rolebook.adamant.traits.4.description'
         }
     ],
     past: [
-        'rolebook.iron.past.0',
-        'rolebook.iron.past.1',
+        'rolebook.adamant.past.0',
+        'rolebook.adamant.past.1',
+        'rolebook.adamant.past.2',
+        'rolebook.adamant.past.3',
+        'rolebook.adamant.past.4'
     ],
     connections: [
-        'rolebook.iron.connections.0',
-        'rolebook.iron.connections.1',
+        'rolebook.adamant.connections.0',
+        'rolebook.adamant.connections.1',
     ],
-    momentOfEquilibrium: "rolebook.iron.momentOfEquilibrium",
+    momentOfEquilibrium: "rolebook.adamant.momentOfEquilibrium",
     combatTechnique: {
-        name: 'rolebook.iron.combatTechnique.name',
-        description: 'rolebook.iron.combatTechnique.description',
+        name: 'rolebook.adamant.combatTechnique.name',
+        description: 'rolebook.adamant.combatTechnique.description',
+        e: true,
+        p: true,
+        g: true,
+        type: CombatTechniqueType.DefenseAndManeuver
+    }
+};
+
+export const ThePillar: Rolebook = {
+    name: "rolebook.pillar",
+    stats: {
+        creativity: 1,
+        focus: 0,
+        harmony: 1,
+        passion: -1
+    },
+    balance: {
+        black: 'rolebook.pillar.balance.black',
+        white: 'rolebook.pillar.balance.white',
+        index: 1
+    },
+    speciality: {
+        name: 'rolebook.pillar.speciality.name',
+        description: 'rolebook.pillar.speciality.description'
+    },
+    traits: [
+        {
+            name: 'rolebook.pillar.traits.0.name',
+            description: 'rolebook.pillar.traits.0.description'
+        },
+        {
+            name: 'rolebook.pillar.traits.1.name',
+            description: 'rolebook.pillar.traits.1.description'
+        },
+        {
+            name: 'rolebook.pillar.traits.2.name',
+            description: 'rolebook.pillar.traits.2.description'
+        },
+        {
+            name: 'rolebook.pillar.traits.3.name',
+            description: 'rolebook.pillar.traits.3.description'
+        },
+        {
+            name: 'rolebook.pillar.traits.4.name',
+            description: 'rolebook.pillar.traits.4.description'
+        }
+    ],
+    past: [
+        'rolebook.pillar.past.0',
+        'rolebook.pillar.past.1',
+        'rolebook.pillar.past.2',
+        'rolebook.pillar.past.3',
+        'rolebook.pillar.past.4'
+    ],
+    connections: [
+        'rolebook.pillar.connections.0',
+        'rolebook.pillar.connections.1',
+    ],
+    momentOfEquilibrium: "rolebook.pillar.momentOfEquilibrium",
+    combatTechnique: {
+        name: 'rolebook.pillar.combatTechnique.name',
+        description: 'rolebook.pillar.combatTechnique.description',
+        e: true,
+        p: true,
+        g: true,
+        type: CombatTechniqueType.DefenseAndManeuver
+    }
+};
+
+export const TheBold: Rolebook = {
+    name: "rolebook.bold",
+    stats: {
+        creativity: 1,
+        focus: 1,
+        harmony: 0,
+        passion: -1
+    },
+    balance: {
+        black: 'rolebook.bold.balance.black',
+        white: 'rolebook.bold.balance.white',
+        index: 1
+    },
+    speciality: {
+        name: 'rolebook.bold.speciality.name',
+        description: 'rolebook.bold.speciality.description'
+    },
+    traits: [
+        {
+            name: 'rolebook.bold.traits.0.name',
+            description: 'rolebook.bold.traits.0.description'
+        },
+        {
+            name: 'rolebook.bold.traits.1.name',
+            description: 'rolebook.bold.traits.1.description'
+        },
+        {
+            name: 'rolebook.bold.traits.2.name',
+            description: 'rolebook.bold.traits.2.description'
+        },
+        {
+            name: 'rolebook.bold.traits.3.name',
+            description: 'rolebook.bold.traits.3.description'
+        },
+        {
+            name: 'rolebook.bold.traits.4.name',
+            description: 'rolebook.bold.traits.4.description'
+        }
+    ],
+    past: [
+        'rolebook.bold.past.0',
+        'rolebook.bold.past.1',
+        'rolebook.bold.past.2',
+        'rolebook.bold.past.3',
+        'rolebook.bold.past.4'
+    ],
+    connections: [
+        'rolebook.bold.connections.0',
+        'rolebook.bold.connections.1',
+    ],
+    momentOfEquilibrium: "rolebook.bold.momentOfEquilibrium",
+    combatTechnique: {
+        name: 'rolebook.bold.combatTechnique.name',
+        description: 'rolebook.bold.combatTechnique.description',
+        e: true,
+        p: true,
+        g: true,
+        type: CombatTechniqueType.DefenseAndManeuver
+    }
+};
+
+export const TheGuardian: Rolebook = {
+    name: "rolebook.guardian",
+    stats: {
+        creativity: -1,
+        focus: 1,
+        harmony: 0,
+        passion: 1
+    },
+    balance: {
+        black: 'rolebook.guardian.balance.black',
+        white: 'rolebook.guardian.balance.white',
+        index: 1
+    },
+    speciality: {
+        name: 'rolebook.guardian.speciality.name',
+        description: 'rolebook.guardian.speciality.description'
+    },
+    traits: [
+        {
+            name: 'rolebook.guardian.traits.0.name',
+            description: 'rolebook.guardian.traits.0.description'
+        },
+        {
+            name: 'rolebook.guardian.traits.1.name',
+            description: 'rolebook.guardian.traits.1.description'
+        },
+        {
+            name: 'rolebook.guardian.traits.2.name',
+            description: 'rolebook.guardian.traits.2.description'
+        },
+        {
+            name: 'rolebook.guardian.traits.3.name',
+            description: 'rolebook.guardian.traits.3.description'
+        },
+        {
+            name: 'rolebook.guardian.traits.4.name',
+            description: 'rolebook.guardian.traits.4.description'
+        }
+    ],
+    past: [
+        'rolebook.guardian.past.0',
+        'rolebook.guardian.past.1',
+        'rolebook.guardian.past.2',
+        'rolebook.guardian.past.3',
+        'rolebook.guardian.past.4'
+    ],
+    connections: [
+        'rolebook.guardian.connections.0',
+        'rolebook.guardian.connections.1',
+    ],
+    momentOfEquilibrium: "rolebook.guardian.momentOfEquilibrium",
+    combatTechnique: {
+        name: 'rolebook.guardian.combatTechnique.name',
+        description: 'rolebook.guardian.combatTechnique.description',
+        e: true,
+        p: true,
+        g: true,
+        type: CombatTechniqueType.DefenseAndManeuver
+    }
+};
+
+export const TheHammer: Rolebook = {
+    name: "rolebook.hammer",
+    stats: {
+        creativity: 1,
+        focus: -1,
+        harmony: 0,
+        passion: 1
+    },
+    balance: {
+        black: 'rolebook.hammer.balance.black',
+        white: 'rolebook.hammer.balance.white',
+        index: 1
+    },
+    speciality: {
+        name: 'rolebook.hammer.speciality.name',
+        description: 'rolebook.hammer.speciality.description'
+    },
+    traits: [
+        {
+            name: 'rolebook.hammer.traits.0.name',
+            description: 'rolebook.hammer.traits.0.description'
+        },
+        {
+            name: 'rolebook.hammer.traits.1.name',
+            description: 'rolebook.hammer.traits.1.description'
+        },
+        {
+            name: 'rolebook.hammer.traits.2.name',
+            description: 'rolebook.hammer.traits.2.description'
+        },
+        {
+            name: 'rolebook.hammer.traits.3.name',
+            description: 'rolebook.hammer.traits.3.description'
+        },
+        {
+            name: 'rolebook.hammer.traits.4.name',
+            description: 'rolebook.hammer.traits.4.description'
+        }
+    ],
+    past: [
+        'rolebook.hammer.past.0',
+        'rolebook.hammer.past.1',
+        'rolebook.hammer.past.2',
+        'rolebook.hammer.past.3',
+        'rolebook.hammer.past.4'
+    ],
+    connections: [
+        'rolebook.hammer.connections.0',
+        'rolebook.hammer.connections.1',
+    ],
+    momentOfEquilibrium: "rolebook.hammer.momentOfEquilibrium",
+    combatTechnique: {
+        name: 'rolebook.hammer.combatTechnique.name',
+        description: 'rolebook.hammer.combatTechnique.description',
+        e: true,
+        p: true,
+        g: true,
+        type: CombatTechniqueType.DefenseAndManeuver
+    }
+};
+
+export const TheIcon: Rolebook = {
+    name: "rolebook.icon",
+    stats: {
+        creativity: 0,
+        focus: 1,
+        harmony: 1,
+        passion: -1
+    },
+    balance: {
+        black: 'rolebook.icon.balance.black',
+        white: 'rolebook.icon.balance.white',
+        index: 1
+    },
+    speciality: {
+        name: 'rolebook.icon.speciality.name',
+        description: 'rolebook.icon.speciality.description'
+    },
+    traits: [
+        {
+            name: 'rolebook.icon.traits.0.name',
+            description: 'rolebook.icon.traits.0.description'
+        },
+        {
+            name: 'rolebook.icon.traits.1.name',
+            description: 'rolebook.icon.traits.1.description'
+        },
+        {
+            name: 'rolebook.icon.traits.2.name',
+            description: 'rolebook.icon.traits.2.description'
+        },
+        {
+            name: 'rolebook.icon.traits.3.name',
+            description: 'rolebook.icon.traits.3.description'
+        },
+        {
+            name: 'rolebook.icon.traits.4.name',
+            description: 'rolebook.icon.traits.4.description'
+        }
+    ],
+    past: [
+        'rolebook.icon.past.0',
+        'rolebook.icon.past.1',
+        'rolebook.icon.past.2',
+        'rolebook.icon.past.3',
+        'rolebook.icon.past.4'
+    ],
+    connections: [
+        'rolebook.icon.connections.0',
+        'rolebook.icon.connections.1',
+    ],
+    momentOfEquilibrium: "rolebook.icon.momentOfEquilibrium",
+    combatTechnique: {
+        name: 'rolebook.icon.combatTechnique.name',
+        description: 'rolebook.icon.combatTechnique.description',
+        e: true,
+        p: true,
+        g: true,
+        type: CombatTechniqueType.DefenseAndManeuver
+    }
+};
+
+export const TheIdealist: Rolebook = {
+    name: "rolebook.idealist",
+    stats: {
+        creativity: 0,
+        focus: -1,
+        harmony: 1,
+        passion: 1
+    },
+    balance: {
+        black: 'rolebook.idealist.balance.black',
+        white: 'rolebook.idealist.balance.white',
+        index: 1
+    },
+    speciality: {
+        name: 'rolebook.idealist.speciality.name',
+        description: 'rolebook.idealist.speciality.description'
+    },
+    traits: [
+        {
+            name: 'rolebook.idealist.traits.0.name',
+            description: 'rolebook.idealist.traits.0.description'
+        },
+        {
+            name: 'rolebook.idealist.traits.1.name',
+            description: 'rolebook.idealist.traits.1.description'
+        },
+        {
+            name: 'rolebook.idealist.traits.2.name',
+            description: 'rolebook.idealist.traits.2.description'
+        },
+        {
+            name: 'rolebook.idealist.traits.3.name',
+            description: 'rolebook.idealist.traits.3.description'
+        },
+        {
+            name: 'rolebook.idealist.traits.4.name',
+            description: 'rolebook.idealist.traits.4.description'
+        }
+    ],
+    past: [
+        'rolebook.idealist.past.0',
+        'rolebook.idealist.past.1',
+        'rolebook.idealist.past.2',
+        'rolebook.idealist.past.3',
+        'rolebook.idealist.past.4'
+    ],
+    connections: [
+        'rolebook.idealist.connections.0',
+        'rolebook.idealist.connections.1',
+    ],
+    momentOfEquilibrium: "rolebook.idealist.momentOfEquilibrium",
+    combatTechnique: {
+        name: 'rolebook.idealist.combatTechnique.name',
+        description: 'rolebook.idealist.combatTechnique.description',
+        e: true,
+        p: true,
+        g: true,
+        type: CombatTechniqueType.DefenseAndManeuver
+    }
+};
+
+export const TheProdigy: Rolebook = {
+    name: "rolebook.prodigy",
+    stats: {
+        creativity: -1,
+        focus: 2,
+        harmony: 0,
+        passion: 0
+    },
+    balance: {
+        black: 'rolebook.prodigy.balance.black',
+        white: 'rolebook.prodigy.balance.white',
+        index: 1
+    },
+    speciality: {
+        name: 'rolebook.prodigy.speciality.name',
+        description: 'rolebook.prodigy.speciality.description'
+    },
+    traits: [
+        {
+            name: 'rolebook.prodigy.traits.0.name',
+            description: 'rolebook.prodigy.traits.0.description'
+        },
+        {
+            name: 'rolebook.prodigy.traits.1.name',
+            description: 'rolebook.prodigy.traits.1.description'
+        },
+        {
+            name: 'rolebook.prodigy.traits.2.name',
+            description: 'rolebook.prodigy.traits.2.description'
+        },
+        {
+            name: 'rolebook.prodigy.traits.3.name',
+            description: 'rolebook.prodigy.traits.3.description'
+        },
+        {
+            name: 'rolebook.prodigy.traits.4.name',
+            description: 'rolebook.prodigy.traits.4.description'
+        }
+    ],
+    past: [
+        'rolebook.prodigy.past.0',
+        'rolebook.prodigy.past.1',
+        'rolebook.prodigy.past.2',
+        'rolebook.prodigy.past.3',
+        'rolebook.prodigy.past.4'
+    ],
+    connections: [
+        'rolebook.prodigy.connections.0',
+        'rolebook.prodigy.connections.1',
+    ],
+    momentOfEquilibrium: "rolebook.prodigy.momentOfEquilibrium",
+    combatTechnique: {
+        name: 'rolebook.prodigy.combatTechnique.name',
+        description: 'rolebook.prodigy.combatTechnique.description',
+        e: true,
+        p: true,
+        g: true,
+        type: CombatTechniqueType.DefenseAndManeuver
+    }
+};
+
+export const TheRogue: Rolebook = {
+    name: "rolebook.rogue",
+    stats: {
+        creativity: 1,
+        focus: 0,
+        harmony: -1,
+        passion: 1
+    },
+    balance: {
+        black: 'rolebook.rogue.balance.black',
+        white: 'rolebook.rogue.balance.white',
+        index: 1
+    },
+    speciality: {
+        name: 'rolebook.rogue.speciality.name',
+        description: 'rolebook.rogue.speciality.description'
+    },
+    traits: [
+        {
+            name: 'rolebook.rogue.traits.0.name',
+            description: 'rolebook.rogue.traits.0.description'
+        },
+        {
+            name: 'rolebook.rogue.traits.1.name',
+            description: 'rolebook.rogue.traits.1.description'
+        },
+        {
+            name: 'rolebook.rogue.traits.2.name',
+            description: 'rolebook.rogue.traits.2.description'
+        },
+        {
+            name: 'rolebook.rogue.traits.3.name',
+            description: 'rolebook.rogue.traits.3.description'
+        },
+        {
+            name: 'rolebook.rogue.traits.4.name',
+            description: 'rolebook.rogue.traits.4.description'
+        }
+    ],
+    past: [
+        'rolebook.rogue.past.0',
+        'rolebook.rogue.past.1',
+        'rolebook.rogue.past.2',
+        'rolebook.rogue.past.3',
+        'rolebook.rogue.past.4'
+    ],
+    connections: [
+        'rolebook.rogue.connections.0',
+        'rolebook.rogue.connections.1',
+    ],
+    momentOfEquilibrium: "rolebook.rogue.momentOfEquilibrium",
+    combatTechnique: {
+        name: 'rolebook.rogue.combatTechnique.name',
+        description: 'rolebook.rogue.combatTechnique.description',
+        e: true,
+        p: true,
+        g: true,
+        type: CombatTechniqueType.DefenseAndManeuver
+    }
+};
+
+export const TheSuccessor: Rolebook = {
+    name: "rolebook.successor",
+    stats: {
+        creativity: 1,
+        focus: 1,
+        harmony: -1,
+        passion: 0
+    },
+    balance: {
+        black: 'rolebook.successor.balance.black',
+        white: 'rolebook.successor.balance.white',
+        index: 1
+    },
+    speciality: {
+        name: 'rolebook.successor.speciality.name',
+        description: 'rolebook.successor.speciality.description'
+    },
+    traits: [
+        {
+            name: 'rolebook.successor.traits.0.name',
+            description: 'rolebook.successor.traits.0.description'
+        },
+        {
+            name: 'rolebook.successor.traits.1.name',
+            description: 'rolebook.successor.traits.1.description'
+        },
+        {
+            name: 'rolebook.successor.traits.2.name',
+            description: 'rolebook.successor.traits.2.description'
+        },
+        {
+            name: 'rolebook.successor.traits.3.name',
+            description: 'rolebook.successor.traits.3.description'
+        },
+        {
+            name: 'rolebook.successor.traits.4.name',
+            description: 'rolebook.successor.traits.4.description'
+        }
+    ],
+    past: [
+        'rolebook.successor.past.0',
+        'rolebook.successor.past.1',
+        'rolebook.successor.past.2',
+        'rolebook.successor.past.3',
+        'rolebook.successor.past.4'
+    ],
+    connections: [
+        'rolebook.successor.connections.0',
+        'rolebook.successor.connections.1',
+    ],
+    momentOfEquilibrium: "rolebook.successor.momentOfEquilibrium",
+    combatTechnique: {
+        name: 'rolebook.successor.combatTechnique.name',
+        description: 'rolebook.successor.combatTechnique.description',
         e: true,
         p: true,
         g: true,
